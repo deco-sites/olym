c2c1f5f3eaeedc8371cf6d2bcf4487a8a147cd6e
ApMatheus <matheusaparecido101@gmail.com>
2024-08-01 20:46:48 -0300

    feat: created Newsletter

--- sections/Newsletter/Newsletter.tsx
+++ sections/Newsletter/Newsletter.tsx
@@ -1,136 +1,63 @@
-import { SectionProps } from "deco/mod.ts";
-import { AppContext } from "../../apps/site.ts";
-import Icon from "../../components/ui/Icon.tsx";
-import Section from "../../components/ui/Section.tsx";
-import { clx } from "../../sdk/clx.ts";
-import { usePlatform } from "../../sdk/usePlatform.tsx";
-import { useComponent } from "../Component.tsx";
+import { RichText } from "apps/admin/widgets.ts";
+import CTAButton, {
+  Props as CTAProps,
+} from "deco-sites/olym/components/Button/CTAButton.tsx";
 
-interface NoticeProps {
-  title?: string;
-  description?: string;
+interface Props {
+  /**
+   * @title Titulo
+   */
+  title: string;
+  /**
+   * @title Descrição
+   */
+  content?: RichText;
+  /**
+   * @title Placeholder
+   */
+  plachegolder: string;
+  /**
+   *  @title Estilizações do CTA
+   */
+  cta: CTAProps;
+  /**
+   *  @title Cor de fundo
+   * @description Cor Padrão: #b3b3b3
+   * @format color-input
+   */
+  background?: string;
 }
 
-export interface Props {
-  empty?: NoticeProps;
-  success?: NoticeProps;
-  failed?: NoticeProps;
+export default function Newsletter(props: Props) {
+  const { title, content, plachegolder, cta, background } = props;
 
-  /** @description Signup label */
-  label?: string;
-
-  /** @description Input placeholder */
-  placeholder?: string;
-
-  /** @hide true */
-  status?: "success" | "failed";
-}
-
-export async function action(props: Props, req: Request, ctx: AppContext) {
-  const platform = usePlatform();
-
-  const form = await req.formData();
-  const email = `${form.get("email") ?? ""}`;
-
-  if (platform === "vtex") {
-    // deno-lint-ignore no-explicit-any
-    await (ctx as any).invoke("vtex/actions/newsletter/subscribe.ts", {
-      email,
-    });
-
-    return { ...props, status: "success" };
-  }
-
-  return { ...props, status: "failed" };
-}
-
-export function loader(props: Props) {
-  return { ...props, status: undefined };
-}
-
-function Notice(
-  { title, description }: { title?: string; description?: string },
-) {
   return (
-    <div class="flex flex-col justify-center items-center sm:items-start gap-4">
-      <span class="text-3xl font-semibold text-center sm:text-start">
+    <div
+      class="flex flex-col w-full h-full bg-[#b3b3b3] px-3 py-5 pb-24 lg:pb-10 lg:pt-0 text-black gap-3 lg:gap-6"
+      style={{ background: background }}
+    >
+      <h2
+        class={" text-[56px] leading-[56px] font-FKOlympikus uppercase lg:text-8xl"}
+      >
         {title}
-      </span>
-      <span class="text-sm font-normal text-base-300 text-center sm:text-start">
-        {description}
-      </span>
+      </h2>
+      {content && (
+        <span
+          class={"text-lg font-Signal leading-[20px] lg:max-w-[690px] mb-9 lg:mb-1 "}
+          dangerouslySetInnerHTML={{ __html: content }}
+        >
+        </span>
+      )}
+      <form class="w-full h-full flex flex-col gap-3 lg:gap-9">
+        <input
+          placeholder={plachegolder}
+          class=" outline-none placeholder:font-Signal lg:text-sm lg:placeholder:text-sm text-lg placeholder:text-lg placeholder:text-black text-black font-Signal border-b border-black w-full lg:max-w-[690px]"
+          style={{ background: background }}
+        >
+        </input>
+
+        <CTAButton {...cta} class="w-fit" />
+      </form>
     </div>
   );
 }
-
-function Newsletter({
-  empty = {
-    title: "Get top deals, latest trends, and more.",
-    description:
-      "Receive our news and promotions in advance. Enjoy and get 10% off your first purchase. For more information click here.",
-  },
-  success = {
-    title: "Thank you for subscribing!",
-    description:
-      "You’re now signed up to receive the latest news, trends, and exclusive promotions directly to your inbox. Stay tuned!",
-  },
-  failed = {
-    title: "Oops. Something went wrong!",
-    description:
-      "Something went wrong. Please try again. If the problem persists, please contact us.",
-  },
-  label = "Sign up",
-  placeholder = "Enter your email address",
-  status,
-}: SectionProps<typeof loader, typeof action>) {
-  if (status === "success" || status === "failed") {
-    return (
-      <Section.Container class="bg-base-200">
-        <div class="p-14 flex flex-col sm:flex-row items-center justify-center gap-5 sm:gap-10">
-          <Icon
-            size={80}
-            class={clx(
-              status === "success" ? "text-success" : "text-error",
-            )}
-            id={status === "success" ? "check-circle" : "error"}
-          />
-          <Notice {...status === "success" ? success : failed} />
-        </div>
-      </Section.Container>
-    );
-  }
-
-  return (
-    <Section.Container class="bg-base-200">
-      <div class="p-14 grid grid-flow-row sm:grid-cols-2 gap-10 sm:gap-20 place-items-center">
-        <Notice {...empty} />
-
-        <form
-          hx-target="closest section"
-          hx-swap="outerHTML"
-          hx-post={useComponent(import.meta.url)}
-          class="flex flex-col sm:flex-row gap-4 w-full"
-        >
-          <input
-            name="email"
-            class="input input-bordered flex-grow"
-            type="text"
-            placeholder={placeholder}
-          />
-
-          <button
-            class="btn btn-primary"
-            type="submit"
-          >
-            <span class="[.htmx-request_&]:hidden inline">
-              {label}
-            </span>
-            <span class="[.htmx-request_&]:inline hidden loading loading-spinner" />
-          </button>
-        </form>
-      </div>
-    </Section.Container>
-  );
-}
-
-export default Newsletter;
